1a3ad8cf79dd8c47622a0613c9494c40413491f4
WeisserSchatten <aeliseey@bk.ru>
2018-02-21 16:13:57 +0300

    Happiness is not a destination. It is a method of life.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 	$('.mobile-toggler').click(function() {
 		$('.header-mobile').toggleClass('-is-open');
+		$('.mobile-toggler i:first-child').toggleClass('-animation_first_i');
+		$('.mobile-toggler i:nth-child(2)').toggleClass('-animation_second_i')
+		$('.mobile-toggler i:last-child').toggleClass('-animation_third_i');
+		$('.mobile-toggler').toggleClass('-animation-mobile-toggler');
 	})
 })
