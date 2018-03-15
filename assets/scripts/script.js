038b325e010fc0e582b98b958b0a2ee0b68677ce
bananaWeb <a89.volkov@gmail.com>
2018-03-15 21:01:29 +0300

    scroll

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,12 +2,18 @@ $(document).ready(function() {
 	let random = Math.floor(Math.random() * (7 - 1) + 1);
   	$(".owl-carousel").owlCarousel({
   		items: 1,
-  		autoHeight: true
+  		autoHeight: true,
+  		nav: true
   	});
-
 	$('.section-main').css({
 		'background-image':'url(./assets/images/bg-' + random + '.jpg)'
 	});
+
+    AOS.init();
+
+    $('.scroll p').click(function(){
+    	$('html,body').animate({ scrollTop: $('.section-portfolio').offset().top }, 500);
+    })
 
 	let modalToggler = $('.js-modal-toggler'),
 		modal = $('.modal'),
